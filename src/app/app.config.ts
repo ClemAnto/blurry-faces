581f57f57b8e5d09f5fa22c93ebbd97fa05a1076
Antonio Clemente <clemente.antonio.81@gmail.com>
2025-05-11 23:23:21 +0200

    removed browser animations for now...

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,17 @@ import { it_IT, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import it from '@angular/common/locales/it';
 import { FormsModule } from '@angular/forms';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+//import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 
 registerLocaleData(it);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzI18n(it_IT), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }), 
+		provideRouter(routes), provideNzI18n(it_IT), 
+		importProvidersFrom(FormsModule), 
+		//provideAnimationsAsync(), 
+		provideHttpClient()
+	]
 };
