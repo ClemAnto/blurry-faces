59031223a7f1a7983a2cd10b79cd21590cce6485
Antonio Clemente <clemente.antonio.81@gmail.com>
2025-05-11 22:36:26 +0200

    restart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  constructor() {}
+  title = 'blurry-faces';
 }
